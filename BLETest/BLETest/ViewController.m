4d5942e0c3edcd9921b5243492d4d50164420f81
chenl_326 <347979248@qq.com>
2016-05-24 14:55:27 +0800

    开始开发

--- BLETest/BLETest/ViewController.m
+++ BLETest/BLETest/ViewController.m
@@ -16,12 +16,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self configUI];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark -
+#pragma mark 初始化UI
+-(void)configUI{
+    NSLog(@"开始开发");
+}
+#pragma mark -
+#pragma mark 加载数据
+
+#pragma mark -
+#pragma mark 事件
+
+#pragma mark -
+#pragma mark 数据请求
+
+#pragma mark -
+#pragma mark 代理
+
+#pragma mark -
+#pragma mark 业务逻辑
+
+#pragma mark -
+#pragma mark 通知注册和销毁
 
 @end
